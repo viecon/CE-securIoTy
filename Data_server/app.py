d54f522faf4f6ec17063cfa544bf875d846bcd29
shawn28e7 <twshawn28e7@gmail.com>
2025-05-16 03:01:45 +0800

    change to multifile

---
+++ Data_server/app.py
@@ -0,0 +1,84 @@
+import requests
+import time
+
+from dataclasses import dataclass
+from flask import Flask, request, jsonify
+
+
+@dataclass
+class EncryptedData:
+    file_name: str
+    key: str
+    file: str
+
+
+app = Flask(__name__)
+
+# morse code per device
+files = {}
+KMS_BASE_URL = "http://localhost:5000/api/verify"
+
+
+def verify_device(uuid, token):
+    if not uuid or not token:
+        return False
+    try:
+        response = requests.post(KMS_BASE_URL, json={"uuid": uuid, "token": token})
+        return response.status_code == 200
+    except Exception as e:
+        app.logger.info(f"Error verifying device: {e}")
+        return False
+
+
+@app.route("/morsecode", methods=["POST"])
+def set_morse():
+    data = request.get_json()
+    file_name = data.get("file_name")
+    uuid = data.get("uuid")
+    uuids = data.get("uuids")
+    keys = data.get("keys")
+    token = data.get("token")
+    code = data.get("code")
+
+    if not verify_device(uuid, token):
+        return "Unauthorized device", 403
+    for i in range(len(uuids)):
+        if uuids[i] not in files:
+            files[uuids[i]] = []
+        files[uuids[i]].append(EncryptedData(file_name, keys[i], code))
+
+    app.logger.info(f"UUID: {uuid}, UUIDS:{uuids},\n Keys: {keys}, Code: {code}")
+
+    return "OK", 200
+
+
+@app.route("/morsecode/get", methods=["POST"])
+def get_morse():
+    data = request.get_json()
+    uuid = data.get("uuid")
+    token = data.get("token")
+    name = data.get("name")
+    app.logger.info(f"UUID: {uuid}, Token: {token}")
+    if not verify_device(uuid, token):
+        return "Unauthorized device", 403
+
+    target = files[uuid].find(lambda x: x.file_name == name)
+
+    return jsonify({"encryptedKey": target.key, "encryptedFile": target.file})
+
+
+@app.route("/getList", methods=["POST"])
+def get_list():
+    data = request.get_json()
+    uuid = data.get("uuid")
+
+    return jsonify({"files": [i.file_name for i in files[uuid]]})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
+
+@app.route("/")
+def home():
+    return "IoT Registration Server Running"
